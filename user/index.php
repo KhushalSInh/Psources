32cf931da5ef525d56fd59db27d8f76baa6e77c3
KhushalSinh <freefireplayer149@gmail.com>
2025-09-04 23:28:37 +0530

    Revert "hosting"
    This reverts commit fd59059eae63ca90fc8cf084a438e8662316d98c.

--- user/index.php
+++ user/index.php
@@ -34,124 +34,7 @@ $res1=$db->getresult();
         </div>
     </section>
 
-    <nav class="navbar navbar-expand-lg">
-    <div class="container">
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <a href="index.html" class="navbar-brand mx-auto mx-lg-0"><span
-                style="color:rgb(30, 212, 212);">P</span>SOURCES</a>
-
-        <div class="d-flex align-items-center d-lg-none">
-        <?php
-                if (isset($_SESSION['role_user']) || isset($_COOKIE['role_user'])) {
-
-                    if ($_SESSION['role_user'] == 'user' || $_COOKIE['role_user'] == 'user') {
-                        ?>
-                    <div class="nav-item dropdown">
-                        <a href="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><img src="image/user.png" alt="" width="40px" height="40px"></a>
-                             <div class="dropdown-menu m-0">
-                                <!-- <a href="#" class="dropdown-item " data-bs-toggle="modal" data-bs-target="#prifile">Myprofile</a> -->
-                                <!-- <a href="#" class="dropdown-item " data-bs-toggle="modal" data-bs-target="#password">Chnage Password</a> -->
-                                <a href="logout.php" class="dropdown-item ">Logout</a>
-                            </div>
-            
-                        </div>
-                        <?php
-
-                    }
-
-                } else {
-                    ?>
-                        <a class="custom-btn btn" href="login.php">
-                            Login
-                        </a>
-                    
-                    <?php
-
-                }
-                ?>
-           </div>
-
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-lg-5">
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="index.php#section_1">Home</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="index.php#section_2">About</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="index.php#section_3">Services</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="index.php#section_4">Pages</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link click-scroll" href="index.php#section_5">Contact</a>
-                </li>
-                <div class="nav-item dropdown">
-                    <a href="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Resouces</a>
-                    <div class="dropdown-menu m-0">
-                    <?php
-                            include './component/connection.php';
-                            $sql="select distinct type from sources";
-                            $result=mysqli_query($con,$sql);
-
-                             while($row=mysqli_fetch_assoc($result))
-                            {?>
-                             <a href="resouces.php?id=<?php echo $row['type']; ?>" class="dropdown-item  "><?php echo $row['type']; ?></a>
-                          <?php
-                        }?>
-                        
-                       </div>
-                </div>
-               
-            </ul>
-
-            <div class="d-lg-flex align-items-center d-none ms-auto " >
-
-                <?php
-                if (isset($_SESSION['role_user']) || isset($_COOKIE['role_user'])) {
-
-                    if ($_SESSION['role_user'] == 'user' || $_COOKIE['role_user'] == 'user') {
-                        ?>
-                    <div class="nav-item dropdown">
-                        <a href="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><img src="image/user.png" alt="" width="40px" height="40px"></a>
-                             <div class="dropdown-menu m-0">
-                                <!-- <a href="#" class="dropdown-item " data-bs-toggle="modal" data-bs-target="#prifile">Myprofile</a> -->
-                                <!-- <a href="#" class="dropdown-item " data-bs-toggle="modal" data-bs-target="#password">Chnage Password</a> -->
-                                <a href="logout.php" class="dropdown-item ">Logout</a>
-                            </div>
-            
-                        </div>
-                        <?php
-
-                    }
-
-                } else {
-                    ?>
-                    <div class="">
-                        <a class="custom-btn btn" href="login.php">
-                            Login
-                        </a>
-                    </div>
-                    <?php
-
-                }
-                ?>
-            </div>
-        </div>
-
-    </div>
-</nav>
+    <?php include './component/navbar.php'; ?>
 
     <main>
 
@@ -189,23 +72,23 @@ $res1=$db->getresult();
             </svg>
         </section>
 
-        <?php include './user/component/about.php'; ?>
+        <?php include './component/about.php'; ?>
 
-        <?php include './user/component/infromation.php'; ?>
+        <?php include './component/infromation.php'; ?>
 
-        <?php include './user/component/popularyt.php'; ?>
+        <?php include './component/popularyt.php'; ?>
         
-        <?php include './user/component/services.php'; ?>
+        <?php include './component/services.php'; ?>
 
-        <?php include './user/component/language.php'; ?>
+        <?php include './component/language.php'; ?>
 
-        <?php include './user/component/contactus.php'; ?>
+        <?php include './component/contactus.php'; ?>
 
     </main>
 
-    <?php include './user/component/footer.php'; ?>
+    <?php include './component/footer.php'; ?>
 
-    <?php include './user/component/js.php'; ?>
+    <?php include './component/js.php'; ?>
 
     <form action="" method="post">
             <div class="modal fade" id="password" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
